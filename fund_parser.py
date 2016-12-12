7c2d07cff80f4a563bc9dce9703bc777e7427c24
panatale1 <panatale1@gmail.com>
2016-12-12 13:26:24 -0500

    start work on file parsing

--- fund_parser.py
+++ fund_parser.py
@@ -2,9 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def mutual_fund_parser(cik_or_ticker):
+def mutual_fund_parser(cik_or_ticker, get_all=False):
 """This method takes in a string CIK or ticker, obtains data,
-   and parses into a tab-delimited file"""
+   and parses into a tab-delimited file
+
+   params:
+   cik_or_ticker: The CIK or ticker to search for, must be a string
+   get_all: A boolean value. If not provided, only the latest 13F record
+            will be retrieved.
+"""
     # Initial search parameters
     payload = {
         'CIK': cik_or_ticker,
@@ -21,7 +27,16 @@ def mutual_fund_parser(cik_or_ticker):
     for line in search_content.findAll(name='tr'):
         if line.findChild(name='td', text='13F-HR'):
             file_urls.append(line.findNext(name='a').attrs['href']
+        if not get_all:
+            break
     # Replace '-index.htm' at the end of the urls with '.txt' to immediately get txt file
     for i in range(len(file_urls)):
         file_urls[i] = file_urls[i].replace('-index.htm', '.txt')
-
+    # Retrieve files and parse
+    for url in file_urls:
+        file_content = requests.get('https://www.sec.gov{0}'.format(url)).content
+        # Two cases here: files with XML and files without XML
+        if '<XML>' in file_content:
+            file_content = file_content.split('</SEC-HEADER>')[1]
+        else:
+            file_content = file_content.split('<PAGE>')[-1]
